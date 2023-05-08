6ddc2fb5ccff42e3a8a5e3909062322ea44550ee
Jason Milkins <jasonm23@gmail.com>
2023-05-08 18:12:13 +0700

    Clean up compose email script

--- bin/compose-email.sh
+++ bin/compose-email.sh
@@ -6,28 +6,31 @@ if [ ! $current_hour -eq 8 ]; then
   echo "Skip Email"
 else
   echo "SEND_MAIL=true" >> "$GITHUB_ENV"
-
-  sqlite3 hn.db < stories.sql > stories.md
-  markdown < stories.md > stories.html
-
-  echo "
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset=\"UTF-8\">
-      <style>
-        $(cat css/style.css)
-      </style>
-    </head>
-    <body>
-       <div class=\"hacker-news-diygest-content\">
-         <h1>HackerNews DIYgest</h1>
-         <p class=\"delivery-date\">$(TZ=Asia/Bangkok date "+Compiled on %Y-%m-%d at %H:%M:%S (Bangkok time)")</p>
-         $(cat stories.html)
-         <p class=\"footnote\">Set up your own HackerNews DIYgest. TODO</p>
-       </div>
-    </body>
-  </html>" > digest.html
-
-  echo "Prepared Email"
 fi
+
+sqlite3 hn.db < stories.sql > stories.md
+markdown < stories.md > stories.html
+
+CSS=$(cat css/style.css)
+STORIES=$(cat stories.html)
+SEND_TIME=$(TZ=Asia/Bangkok date "+Compiled on %Y-%m-%d at %H:%M:%S (Bangkok time)")
+
+cat <<HTML > digest.html
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <style>
+      ${CSS}
+    </style>
+  </head>
+  <body>
+     <div class="hacker-news-diygest-content">
+       <h1>HackerNews DIYgest</h1>
+       <p class="delivery-date">${SEND_TIME}</p>
+       ${STORIES}
+       <p class="footnote">View the [project](https://github.com/codefodder/HackerNews-DIYgest/) to set up your own HackerNews DIYgest.</p>
+     </div>
+  </body>
+</html>
+HTML
