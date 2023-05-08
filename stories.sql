4a43d82aea521fcc5ec7fe4c92607090b9d2bebd
Jason Milkins <jasonm23@gmail.com>
2023-05-08 15:45:47 +0700

    use stories.sql

---
+++ stories.sql
@@ -0,0 +1,6 @@
+SELECT "- [" || title || "](" || link || ")
+  - " || score || " [comments]("|| comment_url || ")"
+FROM stories
+-- WHERE datetime(datetime, '+7 hours') BETWEEN date('now', '-1 day', '+7 hours') AND date('now', 'start of day', '+1 day', '+7 hours')
+ORDER BY CAST(REPLACE(score,' points','') AS INTEGER) DESC, CAST(SUBSTR(datetime, 1, 10) AS INTEGER) DESC
+-- LIMIT 30;
