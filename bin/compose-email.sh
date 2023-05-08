be51656f245c39edc9cc34beffc98b072932fe3c
Jason Milkins <jasonm23@gmail.com>
2023-05-08 21:19:13 +0700

    Fix subject line em-dash

--- bin/compose-email.sh
+++ bin/compose-email.sh
@@ -11,7 +11,7 @@ fi
 SUBJECT_LINE=$(sqlite3 hn.db < top-3-titles.sql |
     PERL_UNICODE=SAL \
     perl -pe 'chomp; if (not eof) { s/$/ \x{2014} / }')
-printf "SUBJECT_LINE=HackerNews DIYgest \x{2014} ${SUBJECT_LINE}" >> "$GITHUB_ENV"
+echo "SUBJECT_LINE=HackerNews DIYgest — ${SUBJECT_LINE}" >> "$GITHUB_ENV"
 
 sqlite3 hn.db < stories.sql > stories.md
 markdown < stories.md > stories.html
