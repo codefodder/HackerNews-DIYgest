37fb92016e51a714f070865481a69309a3bbb119
Jason Milkins <jasonm23@gmail.com>
2023-05-09 20:04:20 +0700

    Email send window 20:00-20:59

--- bin/compose-email.sh
+++ bin/compose-email.sh
@@ -3,8 +3,8 @@
 current_hour=$(TZ=Asia/Bangkok date +%H)
 echo "Current Hour (${current_hour})" >> "$GITHUB_STEP_SUMMARY"
 
-if [ "$current_hour" -ne "19" ]; then
-  echo "Skip Email (${current_hour}) != (19)" >> "$GITHUB_STEP_SUMMARY"
+if [ "$current_hour" -ne "20" ]; then
+  echo "Skip Email (${current_hour}) != (20)" >> "$GITHUB_STEP_SUMMARY"
 else
   echo "SEND_MAIL=true" >> "$GITHUB_ENV"
 fi
