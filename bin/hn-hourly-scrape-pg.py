6c03509ed233738c4e7981828c977209df224e8a
Jason Milkins <jasonm23@gmail.com>
2023-05-12 01:25:25 +0700

    fix imports

--- bin/hn-hourly-scrape-pg.py
+++ bin/hn-hourly-scrape-pg.py
@@ -4,22 +4,10 @@ import os
 import sys
 import ftfy
 import requests
-import dbc
-
 import select
+
+from dbc import *
 from bs4 import BeautifulSoup
-
-def connect_to_db(url):
-    # Connect to PostgreSQL database
-    pg_conn = psycopg.connect(url)
-    pg_cursor = pg_conn.cursor()
-    return pg_conn
-
-def disconnect_from_db(connection, cursor):
-    # commit changes and close the database connection
-    connection.commit()
-    cursor.close()
-    connection.close()
 
 conn = connect_to_db(os.environ['PSQLURL'])
 c = conn.cursor()
