944eccd648a664e9ba9671c529db947cf59bf241
Jason Milkins <jasonm23@gmail.com>
2023-05-11 10:21:56 +0700

    Reorganize, rename, tidy up.

--- bin/compose-subject-line.sh
+++ bin/compose-subject-line.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SUBJECT_LINE=$(sqlite3 hn.db < top-3-titles.sql |
+SUBJECT_LINE=$(sqlite3 hn.db < sql/top-3-titles.sql |
     PERL_UNICODE=SAL \
     perl -pe 'chomp; if (not eof) { s/$/ \x{2014} / }')
 echo "SUBJECT_LINE=HackerNews DIYgest — ${SUBJECT_LINE}" >> "$GITHUB_ENV"
