86a4b64de97ef0ec86688a68548397a73733491c
Jason Milkins <jasonm23@gmail.com>
2023-05-12 14:46:01 +0700

    Simplify SEND_MAIL and record emailSent accordingly

--- bin/email-sent-event.sh
+++ bin/email-sent-event.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+if [[ $SEND_MAIL == "true" ]]; then
 psql $PSQLURL <<HERE
     INSERT INTO event_log (event, value)
     VALUES ('emailSent', '$1');
 HERE
+else
+    echo "Email not sent on this run"
+fi
