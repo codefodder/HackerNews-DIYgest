a9925e55fa17d63b07f2f2f597f3e6deb7cbbe54
Jason Milkins <jasonm23@gmail.com>
2023-05-14 02:33:02 +0700

    Change plaintext generation to use psql format

---
+++ bin/stories.plaintext.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+psql -tAq $PSQLURL <<HERE
+    SET client_encoding TO 'UTF8';
+
+    SELECT
+    format(
+        E'%4s. %s\n      %s\n      %s points // %s\n',
+        ROW_NUMBER()
+        OVER (ORDER BY score DESC, datetime DESC),
+        title,
+        link,
+        score,
+        comment_url)
+
+    FROM stories
+
+    WHERE datetime
+    BETWEEN current_date - interval '1 day'
+    AND current_date
+
+    ORDER BY
+        score DESC,
+        datetime DESC
+        LIMIT 30;
+HERE
