31b4c4d6ea4b30c04b774a32acbfbbf6d80c8e25
jasonm23 <jasonm23@gmail.com>
2024-10-24 16:20:04 +0700

    Remove debug comment

--- bin/hn-hourly-scrape-pg.py
+++ bin/hn-hourly-scrape-pg.py
@@ -103,7 +103,6 @@ for story in stories:
         score = 0
         datetime = subtext_elem.select_one('span.age')['title']
         
-    # Debug logging:
     # datetime content changed on 24-10-2024 
     # It became a dual: ISO8601 DateTime string & Unix Epoch Seconds with 
     # a space separator between the two. e.g. 2024-10-23T08:13:47 1729753343
