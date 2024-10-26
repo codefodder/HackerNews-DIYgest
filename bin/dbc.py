3d73403635de6a04c1f741e642fda0c9c22dfb4c
jasonm23 <jasonm23@gmail.com>
2024-10-26 13:58:02 +0700

    Remove unused cursor in connect to db

--- bin/dbc.py
+++ bin/dbc.py
@@ -3,7 +3,6 @@ import psycopg
 def connect_to_db(url):
     # Connect to PostgreSQL database
     pg_conn = psycopg.connect(url)
-    pg_cursor = pg_conn.cursor()
     return pg_conn
 
 def disconnect_from_db(connection, cursor):
